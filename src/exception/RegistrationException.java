327281c3198c331b2ffd0012e7977467d240dc57
Amrita JaiswaL <kashishamritajaiswal@gmail.com>
2026-03-29 01:32:14 +0530

    Enhance RegistrationException with default and parameterized constructors

--- src/exception/RegistrationException.java
+++ src/exception/RegistrationException.java
@@ -1,7 +1,21 @@
-package exception;
+/*package exception;
 
 public class RegistrationException extends Exception {
     public RegistrationException(String message) {
         super(message);
     }
-}
+}*/
+package exception;
+
+public class RegistrationException extends Exception {
+
+    // Default constructor
+    public RegistrationException() {
+        super("Registration error occurred");
+    }
+
+    // Parameterized constructor
+    public RegistrationException(String message) {
+        super(message);
+    }
+} 
