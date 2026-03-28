05d48b6dee98e6826eec9ff7b6b0953e6889c56d
Amrita JaiswaL <kashishamritajaiswal@gmail.com>
2026-03-29 00:59:43 +0530

    Fix RegistrationManager syntax errors and improve registration logic

--- src/service/RegistrationManager.java
+++ src/service/RegistrationManager.java
@@ -1,3 +1,21 @@
+/*package service;
+
+import model.*;
+import exception.*;
+
+public class RegistrationManager {
+
+    public void register(Student student, Course course) throws RegistrationException {
+
+        if (student.getCourses().contains(course)) {
+            throw new RegistrationException("Already registered for this course!");
+        }
+
+        student.addCourse(course);
+        System.out.println("Course registered successfully!");
+    }
+}*/
+
 package service;
 
 import model.*;
