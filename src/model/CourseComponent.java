1ccdecfef7b192a0fe31e4f1407be6b37e002d6b
Amrita JaiswaL <kashishamritajaiswal@gmail.com>
2026-03-29 17:48:40 +0530

    Updated SubModule class

--- src/model/CourseComponent.java
+++ src/model/CourseComponent.java
@@ -1,20 +1,5 @@
 package model;
 
-/*public abstract class CourseComponent {
-    protected String name;
-
-    public CourseComponent(String name) {
-        this.name = name;
-    }
-
-    public abstract void displayDetails(String indent);
-    public abstract int getCredits();
-
-    public String getName() {
-        return name;
-    }
-}*/
-
 public abstract class CourseComponent {
     protected String name;
 
--- src/service/RegistrationManager.java
+++ src/service/RegistrationManager.java
@@ -1,20 +1,4 @@
-/*package service;
 
-import model.*;
-import exception.*;
-
-public class RegistrationManager {
-
-    public void register(Student student, Course course) throws RegistrationException {
-
-        if (student.getCourses().contains(course)) {
-            throw new RegistrationException("Already registered for this course!");
-        }
-
-        student.addCourse(course);
-        System.out.println("Course registered successfully!");
-    }
-}*/
 
 package service;
 
